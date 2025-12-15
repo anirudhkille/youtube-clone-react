6cb9e50dbb59829072dc65986141559a4c563eaa
anirudhkille <anirudhkille@gmail.com>
2025-12-15 16:30:30 +0530

    removed react player

--- src/pages/video-detail.jsx
+++ src/pages/video-detail.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
-import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import Videos from "../components/videos";
 import Loading from "../components/loading";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const getYoutubeEmbedUrl = (id) => `https://www.youtube.com/embed/${id}?rel=0&modestbranding=1`;
 
 export default function VideoDetail() {
   const [videoDetail, setVideoDetail] = useState(null);
@@ -35,27 +36,35 @@ export default function VideoDetail() {
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", top: "76px", position: "sticky" }}>
-            <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
-              className="react-player"
-              controls
-            />
+            <Box sx={{ position: "relative", paddingTop: "56.25%" }}>
+              <iframe
+                src={getYoutubeEmbedUrl(id)}
+                title="YouTube video player"
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  border: 0,
+                }}
+              />
+            </Box>
+
             <Typography color="#fff" variant="h6" fontWeight="bold" px={2}>
               {title}
             </Typography>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              sx={{ color: "#fff" }}
-              // py={1}
-              px={2}
-            >
+
+            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} px={2}>
               <Link to={`/channel/${channelId}`}>
                 <Typography variant={{ sm: "subtitle1", md: "h5" }} color="#fff">
                   {channelTitle}
                   <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
                 </Typography>
               </Link>
+
               <Stack direction="row" gap="20px" alignItems="center">
                 <Typography variant="body1" sx={{ opacity: 0.7 }}>
                   {parseInt(viewCount).toLocaleString()} views
@@ -67,7 +76,8 @@ export default function VideoDetail() {
             </Stack>
           </Box>
         </Box>
-        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center">
+
+        <Box px={2} py={{ md: 1, xs: 5 }}>
           <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
